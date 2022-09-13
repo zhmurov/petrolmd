0d5ed1267bd10e032f1b3f4680f7d3385555543a
Artem Zhmurov <zhmurov@gmail.com>
2022-09-13 19:23:08 +0300

    Fix create_topologies script so it will work on files with no pairs section
    Also remove headder from the itp files

--- CreateAlkanes/create_topologies.sh
+++ CreateAlkanes/create_topologies.sh
@@ -5,6 +5,20 @@ for filename in *.pdb; do
     name=$(basename $filename .pdb)
     # Run GROMACS to create top files for all the PDBs in the folder
     $GMX pdb2gmx -f $filename -o ${name}.gro -p ${name}.top -i ${name}_posre.itp <<< $'16\n3\n'
-    # Remove data from `[ pairs ]` section as it is required by the TRAPPE-UA forcefield
-    sed -n '1,/pairs/p;/angles/,$p' ${name}.top >> ${name}_nopairs.top
+    # Create a copy of the topology that can be included
+    cp ${name}.top ${name}.itp
+    # Remove th1e header
+    sed -i -n '/\[ moleculetype \]/,$p' ${name}.itp
+    # Remove pairs section (needed by TraPPE forcefield)
+    if grep -Fxq "[ pairs ]" ${name}.itp
+    then
+        sed -i -n '1,/pairs/p;/angles/,$p' ${name}.itp
+        sed -i '\[ pairs \]/d' C8H18.itp
+    else
+        echo "Skipping ${name}.itp"
+    fi
+    # Remove the footer
+    sed -i '/; Include Position restraint file/,$d' ${name}.itp
+    # Rename the molecule
+    sed -i "s/Other/${name}/g" ${name}.itp
 done
