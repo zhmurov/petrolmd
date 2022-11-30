f9f7b22e3a2ccaa79f17eaa9003a06724cf1351b
Artem Zhmurov <zhmurov@gmail.com>
2022-11-30 17:36:50 +0300

    Add script to batch0run minimization on the number of molecules

---
+++ CreateAlkanesCHARMM/minimize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GMX=/usr/local/gromacs/bin/gmx
+
+mkdir coord
+cp ${PETROLMD}/files/em.mdp .
+
+#for filename in *.pdb; do
+filename=C1H4.pdb
+    name=$(basename $filename .pdb)
+    $GMX pdb2gmx -f ${name}.gro -o ${name}.gro -p ${name}.top -i ${name}_posre.itp -ff charmm27 -water tip3p
+    $GMX grompp -f em.mdp -c ${name}.gro -p ${name}.top -o ${name}_em.tpr
+    $GMX mdrun -deffnm ${name}_em
+    cp ${name}_em.gro coord/${name}.gro
+#done
