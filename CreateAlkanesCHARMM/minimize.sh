f6f6e431adf7d3ac6288bdc3630f6352be43144c
Artem Zhmurov <zhmurov@gmail.com>
2022-12-01 11:24:03 +0300

    Change the minimization forcefield to charmm36

--- CreateAlkanesCHARMM/minimize.sh
+++ CreateAlkanesCHARMM/minimize.sh
@@ -7,7 +7,7 @@ cp ${PETROLMD}/files/em.mdp .
 #for filename in *.pdb; do
 filename=C1H4.pdb
     name=$(basename $filename .pdb)
-    $GMX pdb2gmx -f ${name}.gro -o ${name}.gro -p ${name}.top -i ${name}_posre.itp -ff charmm27 -water tip3p
+    $GMX pdb2gmx -f ${name}.gro -o ${name}.gro -p ${name}.top -i ${name}_posre.itp -ff charmm36-jul2021 -water tip3p
     $GMX grompp -f em.mdp -c ${name}.gro -p ${name}.top -o ${name}_em.tpr
     $GMX mdrun -deffnm ${name}_em
     cp ${name}_em.gro coord/${name}.gro
