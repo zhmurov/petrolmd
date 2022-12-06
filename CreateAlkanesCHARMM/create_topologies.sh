5733f400439eec9f06a004affbdf7cf8d8ed0635
Artem Zhmurov <zhmurov@gmail.com>
2022-12-06 14:50:02 +0300

    Combine topology creation and minimization scripts, update docs

--- CreateAlkanesCHARMM/create_topologies.sh
+++ CreateAlkanesCHARMM/create_topologies.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 GMX=/usr/local/gromacs/bin/gmx
+PETROLMD=~/git/artemzhmurov/petrolmd
 
+# Prepare output folders
+mkdir coord
 mkdir toppar
+
+# Copy the energy minimization script
+cp ${PETROLMD}/CreateAlkanesCHARMM/files/em.mdp .
+# Generate PDBs for alkanes
+${PETROLMD}/build/CreateAlkanesCHARMM/create_alkanes_charmm
+# Copy oter PDBs
+cp ${PETROLMD}/CreateAlkanesCHARMM/files/PDBs/* .
 
 for filename in *.pdb; do
     name=$(basename $filename .pdb)
+
+    # Create topology
+
     # Run GROMACS to create top files for all the PDBs in the folder
     $GMX pdb2gmx -f $filename -o ${name}.gro -p ${name}.top -i ${name}_posre.itp -ff charmm36 -water tip3p
     # Create a copy of the topology that can be included
@@ -19,4 +32,19 @@ for filename in *.pdb; do
     # cat ${name}.itp >> alkanes.itp
     # Copy the topolgy to separate folder
     cp ${name}.itp toppar/${name}.itp
+    
+    # Minimize energy
+    
+    # Move molecule so that all coordinates are positive
+    $GMX editconf -f ${name}.pdb -o ${name}.gro -d 0.1
+    # Create lare simulation box (~no PBC simulations)
+    $GMX editconf -f ${name}.gro -o ${name}.gro -box 100 100 100 -noc
+    # Configure and run GROMACS
+    $GMX grompp -f em.mdp -c ${name}.gro -p ${name}.top -o ${name}_em.tpr
+    $GMX mdrun -deffnm ${name}_em
+    # Re-run minimization to produce coordinates in the PDB format
+    $GMX mdrun -deffnm ${name}_em -c ${name}.pdb
+    # Copy the resulting coordinates
+    cp ${name}_em.gro coord/${name}.gro
+    cp ${name}.pdb coord/${name}.pdb
 done
--- CreateAlkanesCHARMM/index.rst
+++ CreateAlkanesCHARMM/index.rst
@@ -3,15 +3,10 @@ Create topologies and coordinates for the alkanes in CHARMM36
 
 Make sure to use patched CHARMM36 forcefield from here: https://gitlab.com/artemzhmurov/charmm36 . 
 
-The following will create initial coordinates for alkanes, build the topologies and minimize coordinates using GROMACS:
+The following will create initial coordinates for alkanes, build the topologies and minimize coordinates using GROMACS. Make sure that you change the path to GROMACS and to this repo in the script before running.
 
     .. code-block:: shell
 
-        mkdir toppar
-        cd toppar
-        ${PETROLMD}/build/CreateAlkanesCHARMM/create_alkanes_charmm
-        cp ${PETROLMD}/CreateAlkanesCHARMM/files/PDBs/* .
         bash ${PETROLMD}/CreateAlkanesCHARMM/create_topologies.sh
-        bash ${PETROLMD}/CreateAlkanesCHARMM/minimize.sh
 
 This will produce two folders: coord, with minimized coordinates in gro and pdb formats and toppar with itp files for the molecules.
