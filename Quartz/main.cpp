fde8db68974177fc275c45767354427c1fe35755
Artem Zhmurov <zhmurov@gmail.com>
2023-02-02 13:41:15 +0300

    WIP: Quartz slab, do transfer atoms into the same box
    Use GROMACS data structures and copy-paste codes from the GROMACS
    source. Does not work yet.

--- Quartz/main.cpp
+++ Quartz/main.cpp
@@ -22,6 +22,106 @@ struct Atom
     int iy;
 };
 
+#define DIM 3
+#define XX 0
+#define YY 1
+#define ZZ 2
+
+typedef float matrix[DIM][DIM];
+
+typedef float rvec[DIM];
+
+void createBoxMatrix(matrix box, const float a, const float b, const float c, const float alpha, const float beta, const float gamma)
+{
+    float cosalphastar = (cosf(beta)*cosf(gamma) - cosf(alpha)) / (sinf(beta)*sin(gamma));
+    float sinalphastar = sqrtf(1 - cosalphastar*cosalphastar);
+
+    box[XX][XX] = a;
+    box[XX][YY] = b*cosf(gamma);
+    box[XX][ZZ] = c*cosf(beta);
+
+    box[YY][XX] = 0.0;
+    box[YY][YY] = b*sin(gamma);
+    box[YY][ZZ] = -c*sin(beta)*cosalphastar;
+
+    box[ZZ][XX] = 0.0;
+    box[ZZ][YY] = 0.0;
+    box[ZZ][ZZ] = c*sin(beta)*sinalphastar;
+}
+
+void calcBoxCenter(const matrix box, rvec center)
+{
+    center[XX] = 0.0;
+    center[YY] = 0.0;
+    center[ZZ] = 0.0;
+    for (int m = 0; m < DIM; m++)
+    {
+        for (int d = 0; d < DIM; d++)
+        {
+            center[d] += 0.5 * box[m][d];
+        }
+    }
+}
+
+
+void putAtomInTriclinicUC(const matrix box, rvec r, const rvec center)
+{
+    /* The product of matrix shm with a coordinate gives the shift vector
+       which is required determine the periodic cell position */
+    float shm01 = box[1][0] / box[1][1];
+    float shm02 = (box[1][1] * box[2][0] - box[2][1] * box[1][0]) / (box[1][1] * box[2][2]);
+    float shm12 = box[2][1] / box[2][2];
+
+    rvec shiftCenter;
+    shiftCenter[XX] = 0.0;
+    shiftCenter[YY] = 0.0;
+    shiftCenter[ZZ] = 0.0;
+    for (int d1 = 0; d1 < DIM; d1++)
+    {
+        shiftCenter[d1] = 0;
+        for (int d2 = 0; d2 < DIM; d2++)
+        {
+            shiftCenter[d1] += box[d2][d1];
+        }
+    }
+    for (int d = 0; d < DIM; d++)
+    {
+        shiftCenter[d] *= 0.5;
+        shiftCenter[d] -= center[d];
+    }
+
+    shiftCenter[0] = shm01 * shiftCenter[1] + shm02 * shiftCenter[2];
+    shiftCenter[1] = shm12 * shiftCenter[2];
+    shiftCenter[2] = 0;
+
+    for (int m = DIM - 1; m >= 0; m--)
+    {
+        float shift = shiftCenter[m];
+        if (m == 0)
+        {
+            shift += shm01 * r[1] + shm02 * r[2];
+        }
+        else if (m == 1)
+        {
+            shift += shm12 * r[2];
+        }
+        while (r[m] - shift < 0)
+        {
+            for (int d = 0; d <= m; d++)
+            {
+                r[d] += box[m][d];
+            }
+        }
+        while (r[m] - shift >= box[m][m])
+        {
+            for (int d = 0; d <= m; d++)
+            {
+                r[d] -= box[m][d];
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 8)
@@ -54,31 +154,20 @@ int main(int argc, char *argv[])
 
     std::cout << a << "  " << b << "  " << c << "  " << alpha << "  " << beta << "  " << gamma << std::endl;
 
+    alpha *= M_PI/180.0;
+    beta *= M_PI/180.0;
+    gamma *= M_PI/180.0;
+
     int Nx = atoi(argv[5]);
     int Ny = atoi(argv[6]);
     float Lz = atof(argv[7]);
     int Nz = ceil(Lz/c);
 
-    alpha *= M_PI/180.0;
-    beta *= M_PI/180.0;
-    gamma *= M_PI/180.0;
+    matrix box;
+    createBoxMatrix(box, a*Nx, b*Ny, c*Nz, alpha, beta, gamma);
 
-    float cosalphastar = (cosf(beta)*cosf(gamma) - cosf(alpha)) / (sinf(beta)*sin(gamma));
-    float sinalphastar = sqrtf(1 - cosalphastar*cosalphastar);
-
-    float xx = a;
-    float xy = b*cosf(gamma);
-    float xz = c*cosf(beta);
-
-    float yx = 0.0;
-    float yy = b*sin(gamma);
-    float yz = -c*sin(beta)*cosalphastar;
-
-    float zx = 0.0;
-    float zy = 0.0;
-    float zz = c*sin(beta)*sinalphastar;
-
-
+    matrix uc;
+    createBoxMatrix(uc, a, b, c, alpha, beta, gamma);
 
     std::cout << "Recommended x - y box size is " << a*Nx << " - " << b*Ny << std::endl;
 
@@ -100,9 +189,9 @@ int main(int argc, char *argv[])
                 float z = xyzIn.atoms[i].z;
 
                 int iz = 0;
-                xyzOut.atoms[iOut].x = x + xx*ix + xy*iy + xz*iz;
-                xyzOut.atoms[iOut].y = y + yx*ix + yy*iy + yz*iz;
-                xyzOut.atoms[iOut].z = z + zx*ix + zy*iy + zz*iz;
+                xyzOut.atoms[iOut].x = x + uc[XX][XX]*ix + uc[XX][YY]*iy + uc[XX][ZZ]*iz;
+                xyzOut.atoms[iOut].y = y + uc[YY][XX]*ix + uc[YY][YY]*iy + uc[YY][ZZ]*iz;
+                xyzOut.atoms[iOut].z = z + uc[ZZ][XX]*ix + uc[ZZ][YY]*iy + uc[ZZ][ZZ]*iz;
 
             }
         }
@@ -173,36 +262,36 @@ int main(int argc, char *argv[])
     fprintf(groOut, "Quartz slab (%dx%d)\n", Nx, Ny);
     fprintf(groOut, "%d\n", static_cast<int>(atomsOut.size()));
     idx = 0;
+
+    rvec center;
+    calcBoxCenter(box, center);
     for (auto atomOut : atomsOut)
     {
+        rvec r;
+        r[XX] = atomOut.x;
+        r[YY] = atomOut.y;
+        r[ZZ] = atomOut.z;
         if (dopbc)
         {
-            atomOut.x -= 0.5*(xx + xy + xz);
-            atomOut.y -= 0.5*(yx + yy + yz);
-            if (atomOut.x < 0)
-            {
-                atomOut.x += a*Nx;
-            }
-            if (atomOut.y < 0)
-            {
-                atomOut.y += b*Ny;
-            }
-            atomOut.z += 0.5*Lz;
+            r[XX] -= 0.5*a;
+            r[YY] -= 0.5*b;
+            r[ZZ] += 0.5*Lz;
+            putAtomInTriclinicUC(box, r, center);
         }
         fprintf(groOut, "%5d%-5s%5s%5d%8.3f%8.3f%8.3f\n",
             atomOut.ix + Nx*atomOut.iy + 1,
             "Q011",
             atomOut.name.c_str(),
             idx + 1,
-            atomOut.x*0.1, // A to nm
-            atomOut.y*0.1,
-            atomOut.z*0.1);
+            r[XX]*0.1, // A to nm
+            r[YY]*0.1,
+            r[ZZ]*0.1);
         idx++;
     }
     fprintf(groOut, "%f %f %f %f %f %f %f %f %f\n", 
-            xx*Nx*0.1, yy*Ny*0.1, zz*Nz*0.1,
-            xy*Ny*0.1, xz*Nz*0.1,
-            yx*Nx*0.1, yz*Nz*0.1, 
-            zx*Nx*0.1, zy*Ny*0.1);
+            box[XX][XX]*Nx*0.1, box[YY][YY]*Ny*0.1, box[ZZ][ZZ]*Nz*0.1,
+            box[XX][YY]*Ny*0.1, box[XX][ZZ]*Nz*0.1,
+            box[YY][XX]*Nx*0.1, box[YY][ZZ]*Nz*0.1, 
+            box[ZZ][XX]*Nx*0.1, box[ZZ][YY]*Ny*0.1);
     fclose(groOut);
 }
