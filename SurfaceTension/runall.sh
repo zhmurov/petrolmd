4151505b0d80b3e5a3c63d55a14edf24a3333324
Artem Zhmurov <zhmurov@gmail.com>
2023-02-23 13:48:01 +0300

    Modify the running script for surface tension evaluation
    1. Use templates for topology/Packmol input
    2. Use NVT simulations with pre-defined box
    3. Prepare for multidi runs

--- SurfaceTension/runall.sh
+++ SurfaceTension/runall.sh
@@ -4,27 +4,42 @@
 GMX=/usr/local/gromacs/bin/gmx
 PACKMOL=~/git/external/packmol/packmol
 
-SYSTEM_NAME=octane_solv
+SYSTEM_NAME=C8H18
 PETROLMD=~/git/artemzhmurov/petrolmd
 FFHOME=~/git/artemzhmurov/charmm36
 
+# Get the force-field
 cp -r ${FFHOME}/charmm36.ff .
 
-cp ${PETROLMD}/SurfaceTension/files/*.inp .
-cp ${PETROLMD}/SurfaceTension/files/*.top .
-cp ${PETROLMD}/files/mdp-charmm36/* .
+# Create folder for the molecular system
+mkdir ${SYSTEM_NAME}
+cd ${SYSTEM_NAME}
 
-$PACKMOL < ${SYSTEM_NAME}.inp
+# Copy and preapre packmol input
+cp ${PETROLMD}/SurfaceTension/files/template.inp packmol.inp
+sed -i "s/NEWMOLECULENAME/${SYSTEM_NAME}/g" packmol.inp
+
+# Copy and prepare topology file
+cp ${PETROLMD}/SurfaceTension/files/template.top topol.top
+sed -i "s/NEWMOLECULENAME/${SYSTEM_NAME}/g" topol.top
+
+# Get the configuration files
+cp ${PETROLMD}/files/mdp-charmm36/em.mdp .
+cp ${PETROLMD}/files/mdp-charmm36/nvt.mdp .
+
+# Create initial structure with Packmol
+$PACKMOL < packmol.inp
  
-$GMX editconf -f ${SYSTEM_NAME}.pdb -o ${SYSTEM_NAME}.gro -box 10 10 10
-$GMX grompp -f em.mdp -c ${SYSTEM_NAME}.gro -p ${SYSTEM_NAME}.top -o em.tpr
+# Configure and energy-minimize GROMACS
+$GMX editconf -f conf.pdb -o conf.gro -box 40 5 5
+$GMX grompp -f em.mdp -c conf.gro -o em.tpr
 $GMX mdrun -deffnm em
-$GMX grompp -f nvt.mdp -c em.gro -p ${SYSTEM_NAME}.top -o nvt.tpr
-$GMX mdrun -deffnm nvt
-$GMX grompp -f npt.mdp -c nvt.gro -p ${SYSTEM_NAME}.top -o npt.tpr
-$GMX mdrun -deffnm npt -update gpu
-$GMX grompp -f md_iso.mdp -c npt.gro -p ${SYSTEM_NAME}.top -o md_iso.tpr
-$GMX mdrun -deffnm md_iso -update gpu
 
-$GMX energy -f md_iso.edr -xvg none -b 5000 <<< $'Pres-XX\nPres-YY\nPres-ZZ\n\n' -o md_iso.pressure.xvg > md_iso.pressure.out
+# Production run
+$GMX grompp -f nvt.mdp -c em.gro -o nvt.tpr
+$GMX mdrun -deffnm nvt -nsteps 5000000
 
+# Get the pressure tensor components
+$GMX energy -f nvt.edr -xvg none -b 5000 <<< $'Pres-XX\nPres-YY\nPres-ZZ\n\n' -o nvt.pressure.xvg > nvt.pressure.out
+
+cd ..
