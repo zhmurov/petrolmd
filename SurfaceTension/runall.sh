f4aedd8062cc7c3a89526e0656719869a1bb74aa
Artem Zhmurov <zhmurov@gmail.com>
2023-02-25 19:12:10 +0300

    Get the name of the forcefield as an argument for surface tension evaluation

--- SurfaceTension/runall.sh
+++ SurfaceTension/runall.sh
@@ -6,12 +6,17 @@ PACKMOL=~/git/external/packmol/packmol
 
 SYSTEM_NAME=C8H18
 PETROLMD=~/git/artemzhmurov/petrolmd
-FFHOME=~/git/artemzhmurov/charmm36
+CHARMM36FFHOME=~/git/artemzhmurov/charmm36
+TRAPPEUAFFHOME=~/git/external/trappeua
 
-# Get the force-field
-cp -r ${FFHOME}/charmm36.ff .
+# Get the force-fields
+cp -r ${CHARMM36FFHOME}/charmm36.ff .
+cp -r ${TRAPPEUAFFHOME}/trappeua.ff .
 
 hydrocarbons="C4H10_ISO C4H10 C5H12_ISO C5H12 C6H14 C6H6 C7H16 C8H18 C9H20 C10H22 C11H24 C12H26 C13H28 C14H30 C15H32 C16H34 C17H36 C18H38 C19H40 C20H42"
+#hydrocarbons="C10H22"
+
+forcefield=$1
 
 for name in $hydrocarbons; do
 
@@ -22,29 +27,31 @@ for name in $hydrocarbons; do
     # Copy and preapre packmol input
     cp ${PETROLMD}/SurfaceTension/files/template.inp packmol.inp
     sed -i "s/NEWMOLECULENAME/${name}/g" packmol.inp
+    sed -i "s/FORCEFIELD/${forcefield}/g" packmol.inp
 
     # Copy and prepare topology file
     cp ${PETROLMD}/SurfaceTension/files/template.top topol.top
     sed -i "s/NEWMOLECULENAME/${name}/g" topol.top
+    sed -i "s/FORCEFIELD/${forcefield}/g" topol.top
 
     # Get the configuration files
-    cp ${PETROLMD}/files/mdp-charmm36/em.mdp .
-    cp ${PETROLMD}/files/mdp-charmm36/nvt.mdp .
+    cp ${PETROLMD}/files/mdp-${forcefield}/em.mdp .
+    cp ${PETROLMD}/files/mdp-${forcefield}/nvt.mdp .
 
     # Create initial structure with Packmol
     $PACKMOL < packmol.inp
     
     # Configure and energy-minimize GROMACS
     $GMX editconf -f conf.pdb -o conf.gro -box 40 5 5
-    $GMX grompp -f em.mdp -c conf.gro -o em.tpr
+    $GMX grompp -f em.mdp -c conf.gro -o em.tpr -maxwarn 1
     $GMX mdrun -deffnm em
 
     # Production run
-    $GMX grompp -f nvt.mdp -c em.gro -o nvt.tpr
-    $GMX mdrun -deffnm nvt -nsteps 1000000
+    $GMX grompp -f nvt.mdp -c em.gro -o nvt.tpr -maxwarn 1
+    $GMX mdrun -deffnm nvt -nsteps 5000000
 
     # Get the pressure tensor components
-    $GMX energy -f nvt.edr -xvg none -b 1000 -e 2000 <<< $'Pres-XX\nPres-YY\nPres-ZZ\n\n' -o nvt.pressure.xvg > nvt.pressure.out
+    $GMX energy -f nvt.edr -xvg none -b 5000 -e 10000 <<< $'Pres-XX\nPres-YY\nPres-ZZ\n\n' -o nvt.pressure.xvg > nvt.pressure.out
 
     # Compute the surface tension
     python3 ${PETROLMD}/SurfaceTension/ComputeSurfaceTension.py > gamma.txt
